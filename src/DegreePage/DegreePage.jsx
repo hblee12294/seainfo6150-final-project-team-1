f458dd5bb7c00f7707777a80b269168a06c25b5b
hblee12294 <hblee12294@gmail.com>
2019-07-30 12:02:16 -0700

    feat: adds degree page

--- src/DegreePage/DegreePage.jsx
+++ src/DegreePage/DegreePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import styles from './DegreePage.module.css'
 
-import Button from '../Button/Button'
+import EnrollLink from '../EnrollLink/EnrollLink'
+
+const COLOR = {
+  beginner: '#16A085',
+  intermediate: '#2E86C1',
+  advanced: '#CB4335',
+  mastery: '#7D3C98'
+}
 
 const DegreePage = ({ degree, schools }) => {
   const thisDegreesSchools = schools.filter(school =>
@@ -16,31 +22,51 @@ const DegreePage = ({ degree, schools }) => {
 
   const syllabusItems =
     degree.syllabus_pdf_urls &&
-    degree.syllabus_pdf_urls.map(url => <li key={url}>{url}</li>)
+    degree.syllabus_pdf_urls.map(url => (
+      <li key={url}>
+        <a
+          className={styles.syllabusLink}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {url}
+        </a>
+      </li>
+    ))
 
   return (
     <div className={styles.wrapper}>
-      <img src={degree.image} alt={degree.title} className={styles.hero} />
-      <section>
-        <p>{degree.short_summary}</p>
-      </section>
-      <section>
-        <h3>Level</h3>
-        <p>{degree.level}</p>
-      </section>
-      <section>
-        <h3>Schools</h3>
-        <ul>{schoolItems}</ul>
-      </section>
-      <section>
-        <h3>Syllabus</h3>
-        <ul>{syllabusItems}</ul>
-      </section>
-      <section>
-        <Link to={`/enroll/${degree.slug}`}>
-          <Button style={{ color: '#3498db' }}>Enroll</Button>
-        </Link>
-      </section>
+      <div className={styles.content}>
+        <div className={styles.head}>
+          <h2 className={styles.title}>
+            {degree.title}{' '}
+            <span
+              className={styles.tag}
+              style={{ color: COLOR[degree.level.toLowerCase().trim()] }}
+            >
+              {degree.level[0].toUpperCase() + degree.level.slice(1).trim()}
+            </span>
+          </h2>
+          <p className={styles.intro}>
+            {degree.short_summary || 'We enter to learn, leave to achieve'}
+          </p>
+          <section className={styles.section}>
+            <h3 className={styles.sectionTitle}>Provided By</h3>
+            <ul className={styles.sectionContent}>{schoolItems}</ul>
+          </section>
+          {syllabusItems ? (
+            <section className={styles.section}>
+              <h3 className={styles.sectionTitle}>Syllabus</h3>
+              <ul className={styles.sectionContent}>{syllabusItems}</ul>
+            </section>
+          ) : null}
+          <section className={styles.section}>
+            <EnrollLink degreeSlug={degree.slug} />
+          </section>
+        </div>
+        <img src={degree.image} alt={degree.title} className={styles.hero} />
+      </div>
     </div>
   )
 }
