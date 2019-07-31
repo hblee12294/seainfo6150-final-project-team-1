bc8860a9040c354cd39e5f8b71f981a2fd5159bb
Yehong Sun <yehongs@kaizhe.ai>
2019-07-31 20:43:02 +0800

    implement error page

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import styles from './ErrorPage.module.css'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>
-      This is the 404 page
+    <div className={styles.wrapper}>
+      <div className={styles.error}>
+        <img
+          className={styles.hero}
+          src="images/computer.jpg"
+          alt="404Error"
+        />
+        <p className={styles.question}>
+          404
+        </p>
+        <p className={styles.question2}>
+          Page not found?
+        </p>
+      </div>
+      <div className={styles.explain}>
+        <p className={styles.explainWords}>
+          Sometimes getting lost isn't that bad.<br></br>
+          Click the menu above or get 
+          back to our &nbsp;
+          <Link
+            to="/"
+            exact
+          >
+            homepage
+          </Link>.
+         </p>
+      </div>
     </div>
   )
 }
